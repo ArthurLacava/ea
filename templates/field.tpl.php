f5cc0d6156c14233803c165f91c2a2d06fc6ccbc
ArthurLacava <38431607+ArthurLacava@users.noreply.github.com>
2018-10-09 13:16:31 -0300

    Adicionado o btn de imprimir
    foi adicionado o botão onde o usuário consegue baixar a pagina em pdf com o verbete e uma observação de como citar o verbete

--- templates/field.tpl.php
+++ templates/field.tpl.php
@@ -56,7 +56,13 @@ HTML comment.
   <?php endif; ?>
   <div class="field-items"<?php print $content_attributes; ?>>
     <?php foreach ($items as $delta => $item): ?>
-      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>><?php print render($item); ?></div>
+      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>><?php print render($item); ?>
+      	
+     <form>
+		<input type="button" value="imprimir verbete" onClick="window.print()"/>
+	</form>
+
+      </div>
     <?php endforeach; ?>
   </div>
 </div>
